5552a65ffc4f296384e110016e3d42a2923d14b0
behnamgolds <29102609+behnamgolds@users.noreply.github.com>
2024-06-03 19:14:03 +0330

    Add code and emblems - Edit Readme

--- README.md
+++ README.md
@@ -1,2 +1,148 @@
 # embleman
-A set of emblem actions built on top of a previous project [numblems](https://github.com/behnamgolds/xfce-thunar-numblems)
+A set of emblem actions built on top of a previous project [embleman](https://github.com/behnamgolds/xfce-thunar-embleman)
+
+
+I just wanted to add some actions to my thunar custom actions ,
+so that I could mark some files/directories with a number or 
+some other emblems and easily increase or decrease the number .
+
+This was intended to be used as thunar custom action, but it
+could also be used on its own like this :
+
+```
+$ python embleman.py < --increse | --decrease | --clock | --check > <file-or-directory-path>
+```
+or
+```
+$ ./embleman.py --help
+```
+But you have to refresh(F5) your thunar window manually.
+If used as thunar custom action it will automatically send
+the F5 key to the active window(aka thunar)
+
+You could also use it in other scripts and just run something
+like the following to send F5 to the active window :
+```
+$ xdotool key F5
+```
+
+I used some svg emblems and put it into my icons directory here :
+```
+~/.icons/Papirus-Dark/symbolic/emblems/
+```
+There are 19 [1..19] svgs since that is what I needed .
+The script will clear the emblem if the number goes out of range .
+I included them in the emblems/ directory in this repo .
+Also the source is mentioned there in emblems/source.txt .
+"Papirus-Dark" is the name of the theme I use, you should change
+it to your theme name and make a symbolic link to this directory
+in your "your-theme-name" directory if you want these emblems to
+be visible in other themes .
+
+then run this to make thunar read the new emblems :
+```
+$ thunar -q
+```
+
+My embleman script is located here :
+```
+~/bin/non-interactive/
+```
+Adding the action to thunar is straightforward from :
+```
+Thunar > Edit > Configure Custom Actions
+```
+![1](https://github.com/behnamgolds/xfce-thunar-embleman/assets/29102609/d28dbfb5-1e21-4536-b536-24476e55b2d3)
+
+The changes will be saved here :
+```
+~/.config/Thunar/uca.xml
+```
+The changes related to embleman for my config will be saved like this :
+```
+</actions>
+<action>
+	<icon>mx-cleanup</icon>
+	<name>Clear</name>
+	<submenu>Emblem</submenu>
+	<unique-id>1702979225176410-1</unique-id>
+	<command>/home/behnam/bin/non-interactive/embleman.py --clear  %F</command>
+	<description>Clear Emblems</description>
+	<range></range>
+	<patterns>*</patterns>
+	<directories/>
+	<audio-files/>
+	<image-files/>
+	<other-files/>
+	<text-files/>
+	<video-files/>
+</action>
+<action>
+	<icon>vcs-normal</icon>
+	<name>Check</name>
+	<submenu>Emblem</submenu>
+	<unique-id>1702646536255992-1</unique-id>
+	<command>/home/behnam/bin/non-interactive/embleman.py --check  %f</command>
+	<description>Add a check mark  to item</description>
+	<range>1</range>
+	<patterns>*</patterns>
+	<directories/>
+	<audio-files/>
+	<image-files/>
+	<other-files/>
+	<text-files/>
+	<video-files/>
+</action>
+<action>
+	<icon>emblem-urgent</icon>
+	<name>Clock</name>
+	<submenu>Emblem</submenu>
+	<unique-id>1706032898158192-1</unique-id>
+	<command>/home/behnam/bin/non-interactive/embleman.py --clock  %f</command>
+	<description>Add a clock mark to item</description>
+	<range>1</range>
+	<patterns>*</patterns>
+	<directories/>
+	<audio-files/>
+	<image-files/>
+	<other-files/>
+	<text-files/>
+	<video-files/>
+</action>
+<action>
+	<icon>list-add</icon>
+	<name>Inc Num</name>
+	<submenu>Emblem</submenu>
+	<unique-id>1717335346720586-1</unique-id>
+	<command>/home/behnam/bin/non-interactive/embleman.py --increase %f</command>
+	<description>Increase Number Emblem</description>
+	<range>*</range>
+	<patterns>*</patterns>
+	<directories/>
+	<audio-files/>
+	<image-files/>
+	<other-files/>
+	<text-files/>
+	<video-files/>
+</action>
+<action>
+	<icon>list-remove</icon>
+	<name>Dec Num</name>
+	<submenu>Emblem</submenu>
+	<unique-id>1717335485659165-2</unique-id>
+	<command>/home/behnam/bin/non-interactive/embleman.py --decrease %f</command>
+	<description>Decrease Number Emblem</description>
+	<range>*</range>
+	<patterns>*</patterns>
+	<directories/>
+	<audio-files/>
+	<image-files/>
+	<other-files/>
+	<text-files/>
+	<video-files/>
+</action>
+</actions>
+```
+
+
+
---
+++ embleman.py
@@ -0,0 +1,186 @@
+#!/usr/bin/python
+
+# https://lazka.github.io/pgi-docs
+# pip install PyGObject
+# pacman -Syu python-gobject
+
+from gi.repository import Gio
+
+import re
+import sys
+import os
+from typing import List, Pattern
+
+# https://github.com/moses-palmer/pynput
+# pip install pynput
+from pynput.keyboard import Key, Controller
+
+import argparse
+from pathlib import Path
+
+app_version = "0.1.0"
+# type Namespace = argparse.Namespace
+
+query_info_flags = Gio.FileQueryInfoFlags(0)
+
+
+class PathResult:
+    def __init__(self, ok=False, path=None):
+        self.ok = ok
+        self.path = path
+
+
+class FsItem:
+    def __init__(self, path: str, action: str, disable_refresh=False):
+        self.__disable_refresh = disable_refresh
+        self.__path = Gio.file_new_for_path(path)
+        self.__action = action
+        self.__path_info = self.__path.query_info('metadata::emblems', query_info_flags)
+        self.__emblems = self.__path_info.get_attribute_stringv("metadata::emblems")
+        self.__emb_num = -1
+        self.__num_emblem_found = False
+
+    def __refresh_window(self):
+        if not self.__disable_refresh:
+            Controller().tap(Key.f5)
+
+    def __matches_pattern(self, pattern: Pattern, emblem: str) -> bool:
+        # TODO: Check this later
+        return pattern.match(emblem) is not None
+
+    def __increase_num(self, n) -> int:
+        if n == 19:
+            return -1
+        return n + 1
+
+    def __decrease_num(self, n) -> int:
+        if n == 1:
+            return -1
+        return n - 1
+
+    def __is_increase_action(self, a: str) -> bool:
+        return a == "inc"
+
+    def __set_one(self):
+        one_emblem = "emblem-num-" + str(1) + "-symbolic"
+        self.__emblems.append(one_emblem)
+        self.__set_emblems()
+
+    def __set_emblems(self):
+        self.__path_info.set_attribute_stringv("metadata::emblems", self.__emblems)
+        self.__path.set_attributes_from_info(self.__path_info, query_info_flags)
+        self.__refresh_window()
+
+    def __execute_num_action(self):
+        for i in range(0, len(self.__emblems)):
+            pattern = re.compile("^emblem-num-[0-9]+-symbolic$")
+            if self.__matches_pattern(pattern, self.__emblems[i]):
+                self.__num_emblem_found = True
+                num = int(self.__emblems[i].split("-")[2])
+
+                if self.__is_increase_action(self.__action):
+                    num = self.__increase_num(num)
+                else:
+                    num = self.__decrease_num(num)
+
+                if num == -1:
+                    self.__emblems.pop(i)
+                    self.__set_emblems()
+                    return
+                else:
+                    new_num_emblem = "emblem-num-" + str(num) + "-symbolic"
+                    self.__emblems[i] = new_num_emblem
+                    self.__set_emblems()
+                    return
+
+        if not self.__num_emblem_found:
+            if self.__is_increase_action(self.__action):
+                self.__set_one()
+                return
+
+    # toggles clock/check emblem on / off
+    def __execute_toggle_action(self):
+        if self.__action == "clock":
+            emblem = "emblem-urgent"
+        else:
+            emblem = "vcs-normal"
+
+        for i in range(0, len(self.__emblems)):
+            if self.__emblems[i] == emblem:
+                self.__emblems.pop(i)
+                self.__set_emblems()
+                return
+        self.__emblems.append(emblem)
+        self.__set_emblems()
+
+    # clears all emblems
+    def __execute_clear_action(self):
+        self.__emblems = []
+        self.__set_emblems()
+
+    def execute_action(self):
+        match self.__action:
+            case "inc" | "dec":
+                self.__execute_num_action()
+            case "clock" | "check":
+                self.__execute_toggle_action()
+            case "clear":
+                self.__execute_clear_action()
+
+
+def init_cmd_parser() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        prog="embleman",
+        description="%(prog)s: a set of emblem actions",
+        epilog="Hey noob ! o_O"
+    )
+
+    parser.add_argument(
+        "-v", f"--version", action="version", version=f"%(prog)s {app_version}"
+    )
+
+    exclusive_action_group = parser.add_mutually_exclusive_group(required=True)
+
+    exclusive_action_group.add_argument("--increase", action="store_const", const="inc",
+                                        dest="action", help="Increase the number emblem")
+    exclusive_action_group.add_argument("--decrease", action="store_const", const="dec",
+                                        dest="action", help="Decrease the number emblem")
+    exclusive_action_group.add_argument("--clock", action="store_const", const="clock",
+                                        dest="action", help="Toggle clock sign emblem")
+    exclusive_action_group.add_argument("--check", action="store_const", const="check",
+                                        dest="action", help="Toggle check sign emblem")
+    exclusive_action_group.add_argument("--clear", action="store_const",
+                                        const="clear", dest="action", help="Clear all the emblems")
+
+    parser.add_argument("paths", nargs="+",
+                        help="Path or Paths, Only --clear action takes multiple paths")
+    parser.add_argument("--disable-refresh", action="store_true", dest="disable_refresh",
+                        default=False, help="Disables Sending F5 Key, useful when running from command line")
+
+    return parser.parse_args()
+
+
+def path_exists(path: str) -> PathResult:
+    p = Path(path)
+    if p.exists():
+        return PathResult(True, str(p.absolute()))
+    return PathResult()
+
+
+def main():
+    args = init_cmd_parser()
+    if args.action == "clear":
+        for path in args.paths:
+            result = path_exists(path)
+            if result.ok:
+                item = FsItem(result.path, args.action, args.disable_refresh)
+                item.execute_action()
+    else:
+        result = path_exists(args.paths[0])
+        if result.ok:
+            item = FsItem(result.path, args.action, args.disable_refresh)
+            item.execute_action()
+
+
+if __name__ == "__main__":
+    main()
